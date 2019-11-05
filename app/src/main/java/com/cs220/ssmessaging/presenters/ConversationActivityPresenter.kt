227c75a290c9a99e19f98868f3f8bd315812c5ff
Zene Sekou <zene@cs.uchicago.edu>
2019-11-05 15:06:02 -0600

    conversation presenter wip

---
+++ app/src/main/java/com/cs220/ssmessaging/presenters/ConversationActivityPresenter.kt
@@ -0,0 +1,24 @@
+package com.cs220.ssmessaging.presenters
+
+class ConversationActivityPresenter : BasePresenter<ConversationActivityPresenter.View>() {
+
+    fun sendTextMessage(text: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun sendImageMessage(image: Array<Byte>){
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun deleteSentTextMessage(text: String){
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun deleteSentImageMessage(image: Array<Byte>){
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    interface View {
+        fun updateDisplayedMessages()
+    }
+}
