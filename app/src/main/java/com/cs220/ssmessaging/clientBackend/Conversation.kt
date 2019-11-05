1089accf16d2131a0ca9e0448c6c247c7a6e7f3d
Troy Hu <hut@uchicago.edu>
2019-11-05 10:17:37 -0600

    Conversation and Device skeleton source files

--- app/src/main/java/com/cs220/ssmessaging/clientBackend/Conversation.kt
+++ app/src/main/java/com/cs220/ssmessaging/clientBackend/Conversation.kt
@@ -1,4 +1,47 @@
 package com.cs220.ssmessaging.clientBackend
+import com.cs220.ssmessaging.clientBackend.User
+import com.cs220.ssmessaging.clientBackend.Message
 
-class Conversation {
+class Conversation() {
+    /* Note, all accesses (except for the constructor) in Kotlin must go through the getter and setter.
+     * This is because variables are properties and all properties have a private field.
+     * Therefore we achieve encapsulation Kotlin by defining custom getters and setters.
+     * That is why in most cases we don't need to declare private variables.
+     */
+    var user1 : User
+        get(){
+            // TODO
+            return User()
+        }
+        set(user : User) {
+        }
+
+
+    var user2 : User
+        get() {
+            // TODO
+            return User()
+        }
+        set(user : User){
+            // TODO
+        }
+
+    var messages : MutableList<Message>
+        get() {
+            // TODO
+            return mutableListOf()
+        }
+        private set(messageList){
+            // TODO
+        }
+
+    fun addMessage(msg : Message) : Boolean{
+        // TODO
+        return false
+    }
+
+    fun sync() : Boolean{
+        // TODO
+        return false
+    }
 }
--- app/src/main/java/com/cs220/ssmessaging/clientBackend/Device.kt
+++ app/src/main/java/com/cs220/ssmessaging/clientBackend/Device.kt
@@ -1,4 +1,48 @@
 package com.cs220.ssmessaging.clientBackend
 
-class Device{
+import java.security.PrivateKey
+import java.security.PublicKey
+
+class Device(){
+    // Variables that are non nullable MUST be declared at initialization time in Kotlin.
+    /* Note, all accesses (except for the constructor) in Kotlin must go through the getter and setter.
+     * This is because variables are properties and all properties have a private field.
+     * Therefore we achieve encapsulation Kotlin by defining custom getters and setters.
+     * That is why in most cases we don't need to declare private variables.
+     */
+    var pathToPrivateKey : String
+        get() {
+            // TODO
+            return ""
+        }
+        set(privateKeyInput : String) {
+            // TODO
+        }
+
+    var pathToPublicKey : String
+        get() {
+            // TODO
+            return ""
+        }
+        set(publicKeyInput : String) {
+            // TODO
+        }
+
+    constructor(privateKeyPathInput : String, publicKeyPathInput : String) : this(){
+        // TODO
+    }
+
+    fun getPrivateKey() : PrivateKey? {
+        // TODO
+        return null
+    }
+
+    fun getPublicKey() : PublicKey? {
+        // TODO
+        return null
+    }
+
+    fun generateKeyPair() : Unit {
+        // TODO
+    }
 }
