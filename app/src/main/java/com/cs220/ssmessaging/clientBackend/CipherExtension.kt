1249d4f929e2d81b064a3647e597aaa0d1e73e77
troyhu2 <hut@uchicago.edu>
2019-11-06 14:31:27 -0600

    added more tests for Cipher - almost done with CipherExtension tests

--- app/src/main/java/com/cs220/ssmessaging/clientBackend/CipherExtension.kt
+++ app/src/main/java/com/cs220/ssmessaging/clientBackend/CipherExtension.kt
@@ -1,5 +1,6 @@
 package com.cs220.ssmessaging.clientBackend
 import com.cs220.ssmessaging.clientBackend.Message
+import org.w3c.dom.Text
 import java.security.KeyPairGenerator
 import java.security.PrivateKey
 
@@ -47,13 +48,13 @@ class CipherExtension(privateKey: PrivateKey, publicKeys : MutableMap<String, Pu
         return false
     }
 
-    fun decryptEncryptedMessage(encryptedMsg : EncryptedMessage) : UnencryptedMessage? {
+    fun decryptEncryptedMessage(encryptedMsg : EncryptedMessage) : UnencryptedMessage {
         // TODO
-        return null
+        return TextMessage("", "", User(), User(), 1)
     }
 
-    fun encryptUnencryptedMessage(unencryptedMsg: UnencryptedMessage) : EncryptedMessage? {
+    fun encryptUnencryptedMessage(unencryptedMsg: UnencryptedMessage) : EncryptedMessage {
         // TODO
-        return null
+        return EncryptedMessage(ByteArray(0), "", "", User(), User(), 1)
     }
 }
--- app/src/test/java/com/cs220/ssmessaging/clientBackendUnitTests/CipherExtensionUnitTests.kt
+++ app/src/test/java/com/cs220/ssmessaging/clientBackendUnitTests/CipherExtensionUnitTests.kt
@@ -1,5 +1,6 @@
 package com.cs220.ssmessaging.clientBackendUnitTests
 
+import com.cs220.ssmessaging.clientBackend.*
 import org.junit.Test
 import org.junit.Assert.*
 import org.junit.runner.RunWith
@@ -7,11 +8,13 @@ import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
 
-import com.cs220.ssmessaging.clientBackend.CipherExtension
 import org.junit.Before
+import org.w3c.dom.Text
+import java.lang.Exception
 import java.security.*
 import java.security.spec.PKCS8EncodedKeySpec
 import java.security.spec.X509EncodedKeySpec
+import javax.crypto.Cipher
 
 @RunWith(MockitoJUnitRunner::class)
 class CipherExtensionUnitTests{
@@ -20,6 +23,7 @@ class CipherExtensionUnitTests{
     private val keyPair1 = keyPairGen.generateKeyPair()
     private val keyPair2 = keyPairGen.generateKeyPair()
     private var testPrivateKey : PrivateKey = keyPair1.private
+    private var testPrivateKey2 : PrivateKey = keyPair2.private
     private var testPublicKey : PublicKey = keyPair1.public
     private var testPublicKey2 : PublicKey = keyPair2.public
 
@@ -39,37 +43,105 @@ class CipherExtensionUnitTests{
     }
 
     @Test
-    fun testEncryptUnencryptedMessageText(){
-
+    fun testSetPublicKeyRings(){
+        // Test setting the public key rings for 0 size, 1 size, and multiple size
+        var testCipherExtension =
+            CipherExtension(testPrivateKey, mutableMapOf("person1" to testPublicKey, "person2" to testPublicKey2))
     }
 
     @Test
-    fun testEncryptUnencryptedMessageImage(){
-
+    fun testSetPrivateKey(){
     }
 
     @Test
-    fun testDecryptEncryptedMessageImage(){
-
+    fun testAddPublicKeyToKeyRing(){
+        // Test
     }
 
     @Test
-    fun testDecryptEncryptedMessageText(){
+    fun testEncryptAndDecryptMessageText(){
+        // Tests the case when only one message encrypted and multiple messages encrypted.
+        var testCipherExtension =
+            CipherExtension(testPrivateKey2, mutableMapOf("person1" to testPublicKey, "person2" to testPublicKey2))
 
+        // Create new user
+        var sendUser : User = User("person1", "test", "User")
+        var receiveUser : User = User("person2", "test", "User2")
+        // Create new unencrypted text messages
+        var textMessage : TextMessage = TextMessage("ABC", "testId", sendUser, receiveUser, 0)
+        var textMessage2 : TextMessage = TextMessage("sad46546 ----", "testId2", sendUser, receiveUser, 123)
+
+        var encryptedMessage = testCipherExtension.encryptUnencryptedMessage(textMessage)
+        var encryptedMessage2 = testCipherExtension.encryptUnencryptedMessage(textMessage2)
+
+        // Decrypt and verify
+        var decryptedMessage = testCipherExtension.decryptEncryptedMessage(encryptedMessage)
+        var decryptedMessage2 = testCipherExtension.decryptEncryptedMessage(encryptedMessage2)
+
+        var message : String = (decryptedMessage2 as TextMessage).message
+        var message2 : String = (decryptedMessage as TextMessage).message
+
+        assertEquals("ABC", message)
+        assertEquals("testId", encryptedMessage.conversationId)
+        assertEquals("person1", encryptedMessage.sender.userId)
+        assertEquals("person2", encryptedMessage.sender.userId)
+        assertEquals(0, encryptedMessage.timestamp)
+
+        assertEquals("sad46546 ----", message2)
+        assertEquals("testId2", encryptedMessage2.conversationId)
+        assertEquals("person1", encryptedMessage2.sender.userId)
+        assertEquals("person2", encryptedMessage2.sender.userId)
+        assertEquals(123, encryptedMessage.timestamp)
+    }
+
+    @Test
+    fun testEncryptAndDecryptMessageImage(){
+        // Tests the case when only one message encrypted and multiple messages encrypted.
+        var testCipherExtension =
+            CipherExtension(testPrivateKey2, mutableMapOf("person1" to testPublicKey, "person2" to testPublicKey2))
+
+        // Create new user
+        var sendUser : User = User("person1", "test", "User")
+        var receiveUser : User = User("person2", "test", "User2")
+        // Create new unencrypted text messages
+        var imgMessage : ImageMessage = ImageMessage(ByteArray(10), "testId", sendUser, receiveUser, 0)
+        var imgMessage2 : ImageMessage = ImageMessage(ByteArray(10), "testId2", sendUser, receiveUser, 123)
+
+        var encryptedMessage = testCipherExtension.encryptUnencryptedMessage(imgMessage)
+        var encryptedMessage2 = testCipherExtension.encryptUnencryptedMessage(imgMessage2)
+
+        // Decrypt and verify
+        var decryptedMessage = testCipherExtension.decryptEncryptedMessage(encryptedMessage)
+        var decryptedMessage2 = testCipherExtension.decryptEncryptedMessage(encryptedMessage2)
+
+        var message : ByteArray = (decryptedMessage2 as ImageMessage).message
+        var message2 : ByteArray = (decryptedMessage as ImageMessage).message
+
+        assertEquals(imgMessage.message, message)
+        assertEquals("testId", encryptedMessage?.conversationId)
+        assertEquals("person1", encryptedMessage?.sender?.userId)
+        assertEquals("person2", encryptedMessage?.sender?.userId)
+        assertEquals(0, encryptedMessage?.timestamp)
+
+        assertEquals(imgMessage2.message, message2)
+        assertEquals("testId2", encryptedMessage2?.conversationId)
+        assertEquals("person1", encryptedMessage2?.sender?.userId)
+        assertEquals("person2", encryptedMessage2?.sender?.userId)
+        assertEquals(123, encryptedMessage?.timestamp)
     }
 
     // The next tests test the case where encryption/decryption can fail
     // (e.g. public key not found in key ring, or message is bad)
-    @Test
+    @Test(expected = Exception::class)
     fun testEncryptUnencryptedMessageFailNoPublicKey(){
     }
 
-    @Test
+    @Test(expected = Exception::class)
     fun testEncryptUnencryptedMessageFailBadMessage(){
         // No Bytes in message
     }
 
-    @Test
+    @Test(expected = Exception::class)
     fun testDecryptEncryptedMessageFailBadMessage(){
     }
 }
