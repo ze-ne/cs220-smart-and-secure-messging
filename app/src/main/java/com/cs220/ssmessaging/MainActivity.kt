90658ea96fcb68092f8ea787c705c71b5abde81a
Dona Ganzorig <ganzorig0518@uchicago.edu>
2019-11-06 15:54:09 -0600

    Some more setup to verify Firebase queries work now

--- app/src/main/java/com/cs220/ssmessaging/MainActivity.kt
+++ app/src/main/java/com/cs220/ssmessaging/MainActivity.kt
@@ -2,11 +2,28 @@ package com.cs220.ssmessaging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        FirebaseFirestore.getInstance().collection("users")
+//            .addSnapshotListener { value, e ->
+//                if (e != null) {
+//                    Log.w(TAG, "Listen failed.", e)
+//                    return@addSnapshotListener
+//                }
+//
+//                for (doc in value!!) {
+//                    Log.d(TAG, doc.id)
+//                }
+//            }
     }
 }
