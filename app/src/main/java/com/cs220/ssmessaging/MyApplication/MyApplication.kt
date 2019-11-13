d27f0a40e9d7e8fb3438af338d821d1b51818276
Troy Hu <hut@uchicago.edu>
2019-11-12 18:03:28 -0600

    moving tests to androidTest (instrumented test)

---
+++ app/src/main/java/com/cs220/ssmessaging/MyApplication/MyApplication.kt
@@ -0,0 +1,18 @@
+package com.cs220.ssmessaging.MyApplication
+
+import android.app.Application
+import android.content.Context
+
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        appContext = applicationContext
+    }
+
+    companion object {
+        var appContext: Context? = null
+            private set
+    }
+}
