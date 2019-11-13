7f7cd2caf85db90c05ce8e75c604f9a555f32ce8
Troy Hu <hut@uchicago.edu>
2019-11-13 00:52:43 -0600

    removed embarrassing commit message

--- app/src/main/java/com/cs220/ssmessaging/clientBackend/User.kt
+++ app/src/main/java/com/cs220/ssmessaging/clientBackend/User.kt
@@ -36,7 +36,6 @@ class User() {
             this.firstName = firstName
             this.lastName = lastName
         }
-        Log.i("KEK", db.toString())
     }
 
     // Additional constructor allows for setting conversations and contacts
