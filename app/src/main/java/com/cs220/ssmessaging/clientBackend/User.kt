62bcb66de17dc7929712711975fb03af0bfa1d31
Connor Hopcraft <chopcraft@uchicago.edu>
2019-11-13 23:14:37 -0600

    decrypted message chain

--- app/src/main/java/com/cs220/ssmessaging/clientBackend/User.kt
+++ app/src/main/java/com/cs220/ssmessaging/clientBackend/User.kt
@@ -207,7 +207,7 @@ class User() {
         if (retConvoList.isEmpty()) {
             return null
         }
-        return retConvoList.single()
+        return retConvoList[0]
     }
 
     fun getConversationByConversationId(convoId: String) : Conversation? {
@@ -217,7 +217,7 @@ class User() {
         if (retConvoList.isEmpty()) {
             return null
         }
-        return retConvoList.single()
+        return retConvoList[0]
     }
 
     fun addContact(user : String) : Boolean {
@@ -233,7 +233,7 @@ class User() {
         if (retUserList.isEmpty()) {
             return null
         }
-        return retUserList.single()
+        return retUserList[0]
     }
 
     fun deleteContact(user : String) : Boolean {
@@ -319,8 +319,8 @@ class User() {
     }
 
     // Handle incoming message from server
-    fun receiveMsg(encryptedMsg: EncryptedMessage) : Boolean {
-        var decryptedMsg = device.cipher.decryptEncryptedMessage(encryptedMsg)
+    fun receiveMsg(decryptedMsg: TextMessage) : Boolean {
+        // var decryptedMessage = device.cipher.decryptEncryptedMessage(encryptedMsg)
         var localConvoObject = getConversationByUserId(decryptedMsg.senderId)
         localConvoObject ?: return false
         localConvoObject ?. addMessage(decryptedMsg)
