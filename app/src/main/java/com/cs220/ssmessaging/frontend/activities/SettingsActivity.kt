95b34392437399d665347da21daf2375d7ab9e59
Dona Ganzorig <ganzorig0518@uchicago.edu>
2019-11-13 19:04:57 -0600

    Code for contacts front-end and settings

--- app/src/main/java/com/cs220/ssmessaging/frontend/activities/SettingsActivity.kt
+++ app/src/main/java/com/cs220/ssmessaging/frontend/activities/SettingsActivity.kt
@@ -1,37 +1,83 @@
 package com.cs220.ssmessaging.frontend.activities
 
+import android.content.Intent
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.cs220.ssmessaging.MyApplication.MyApplication
+import com.google.common.base.Strings.isNullOrEmpty
+import com.google.firebase.auth.FirebaseAuth
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.cs220.ssmessaging.R
 import com.cs220.ssmessaging.frontend.presenters.SettingsActivityPresenter
 
 class SettingsActivity : AppCompatActivity(), SettingsActivityPresenter.View {
+    private val presenter: SettingsActivityPresenter = SettingsActivityPresenter()
+    private lateinit var firstName: String
+    private lateinit var lastName: String
+    private lateinit var changeFirstNameButton: Button
+    private lateinit var changeLastNameButton: Button
+    private lateinit var logoutButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        firstName = (findViewById(R.id.settings_firstName)as EditText).text.toString()
+        lastName = (findViewById(R.id.settings_lastName) as EditText).text.toString()
+
+        changeFirstNameButton = findViewById<Button>(R.id.changeFirstNameButton)
+        changeLastNameButton = findViewById<Button>(R.id.changeLastNameButton)
+        logoutButton = findViewById<Button>(R.id.logoutButton)
+
+        logoutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val registerIntent = Intent(this, LoginActivity::class.java)
+            startActivity(registerIntent)
+
+        }
+
+        changeFirstNameButton.setOnClickListener {
+            if (isNullOrEmpty (firstName))
+                changeFirstNameFail()
+            else
+                MyApplication.currentUser?.firstName = firstName
+            changeFirstNameSuccessful()
+        }
+
+        changeLastNameButton.setOnClickListener {
+            if (isNullOrEmpty (lastName))
+                changeLastNameFail()
+            else
+                MyApplication.currentUser?.lastName = lastName
+            changeLastNameSuccessful()
+        }
     }
 
     override fun logoutSuccessful() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Toast.makeText(this, "Logout Successful", Toast.LENGTH_SHORT).show()
     }
 
     override fun logoutFail() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Toast.makeText(this, "Logout Failed", Toast.LENGTH_SHORT).show()
     }
 
     override fun changeFirstNameSuccessful() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Toast.makeText(this, "First Name Change Successful", Toast.LENGTH_SHORT).show()
     }
 
     override fun changeFirstNameFail() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Toast.makeText(this, "First Name Change Failed", Toast.LENGTH_SHORT).show()
     }
 
     override fun changeLastNameSuccessful() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Toast.makeText(this, "Last Name Change Successful", Toast.LENGTH_SHORT).show()
     }
 
     override fun changeLastNameFail() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Toast.makeText(this, "Last Name Change Failed", Toast.LENGTH_SHORT).show()
     }
 }
