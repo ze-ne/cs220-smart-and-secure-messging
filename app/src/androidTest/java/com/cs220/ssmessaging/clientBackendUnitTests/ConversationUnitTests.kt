0bd74ef39fb8d12002f6c448581f88c18e5a4aca
Dona Ganzorig <ganzorig0518@uchicago.edu>
2019-11-20 14:03:00 -0600

    Add Conversation unit test

--- app/src/androidTest/java/com/cs220/ssmessaging/clientBackendUnitTests/ConversationUnitTests.kt
+++ app/src/androidTest/java/com/cs220/ssmessaging/clientBackendUnitTests/ConversationUnitTests.kt
@@ -1,10 +1,11 @@
 package com.cs220.ssmessaging.clientBackendUnitTests
 
 import com.cs220.ssmessaging.clientBackend.*
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
 import org.json.JSONObject
 import org.junit.Test
 import org.junit.Assert.*
-import org.junit.runner.RunWith
 
 
 class ConversationUnitTests{
@@ -246,5 +247,78 @@ class ConversationUnitTests{
         assertEquals(emptyConversationJSON, emptyConversation.formatConversationData())
         assertEquals(oneSidedConversationJSON, oneSidedConversation.formatConversationData())
         assertEquals(twoSidedConversationJSON, twoSidedConversation.formatConversationData())
+    }
+
+    @Test
+    fun testGetAnalytics() {
+
+        // Since unit tests shouldn't make calls to API and we don't have implementation yet,
+        // test that JSON object allows us to access properties we expect
+
+        val documentTone1 = JSONObject()
+        documentTone1.put("score", 0.6165)
+        documentTone1.put("tone_id", "sadness")
+        documentTone1.put("tone_name", "Sadness")
+
+        val documentTone2 = JSONObject()
+        documentTone2.put("score", 0.829888)
+        documentTone2.put("tone_id", "analytical")
+        documentTone2.put("tone_name", "Analytical")
+
+        val document = JSONObject()
+        document.put("tones", arrayOf(documentTone1, documentTone2))
+
+        val sentenceTone1 = JSONObject()
+        sentenceTone1.put("score", 0.801827)
+        sentenceTone1.put("tone_id", "analytical")
+        sentenceTone1.put("tone_name", "Analytical")
+
+        val sentence1 = JSONObject()
+        sentence1.put("sentence_id", 0)
+        sentence1.put("text", "Team, I know that times are tough!")
+        sentence1.put("tones", arrayOf(sentenceTone1))
+
+        val sentenceTone2 = JSONObject()
+        sentenceTone2.put("score", 0.771241)
+        sentenceTone2.put("tone_id", "sadness")
+        sentenceTone2.put("tone_name", "Sadness")
+
+        val sentenceTone3 = JSONObject()
+        sentenceTone3.put("score", 0.687768)
+        sentenceTone3.put("tone_id", "analytical")
+        sentenceTone3.put("tone_name", "Analytical")
+
+        val sentence2 = JSONObject()
+        sentence2.put("sentence_id", 1)
+        sentence2.put("text", "Product sales have been disappointing for the past three quarters.")
+        sentence2.put("tones", arrayOf(sentenceTone2, sentenceTone3))
+
+        val sentenceTone4 = JSONObject()
+        sentenceTone4.put("score", 0.506763)
+        sentenceTone4.put("tone_id", "analytical")
+        sentenceTone4.put("tone_name", "Analytical")
+
+        val sentence3 = JSONObject()
+        sentence3.put("sentence_id", 2)
+        sentence3.put("text", "We have a competitive product, but we need to do a better job of selling it!")
+        sentence3.put("tones", arrayOf(sentenceTone4))
+
+        val sentences: Array<JSONObject> = arrayOf(sentence1, sentence2, sentence3)
+
+        val mockResponse = JSONObject()
+        mockResponse.put("document_tone", document)
+        mockResponse.put("sentences_tone", sentences)
+
+        val conversationMock = mock<Conversation> {
+            on { getAnalytics() } doReturn mockResponse
+        }
+
+        val response: JSONObject = conversationMock.getAnalytics()
+
+        assertNotNull(response.getJSONObject("document_tone"))
+        assertNotNull(response.getJSONArray("sentences_tone"))
+
+        assertEquals(response.getJSONObject("document_tone").getJSONArray("tones").length(), 2)
+        assertEquals(response.getJSONArray("sentences_tone"), 3)
     }
 }
