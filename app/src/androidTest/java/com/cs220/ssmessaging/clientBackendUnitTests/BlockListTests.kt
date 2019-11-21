6724e74f3feef7bc11c7a30d431ed760833d8857
Connor Hopcraft <chopcraft@uchicago.edu>
2019-11-20 18:08:59 -0600

    blocking tests added

--- app/src/androidTest/java/com/cs220/ssmessaging/clientBackendUnitTests/BlockListTests.kt
+++ app/src/androidTest/java/com/cs220/ssmessaging/clientBackendUnitTests/BlockListTests.kt
@@ -10,6 +10,7 @@ import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
 import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.Test
 import java.nio.charset.Charset
 import java.security.KeyFactory
@@ -61,11 +62,59 @@ class BlockListUnitTests{
     }
 
     @Test
-    fun testautoRemoveContact{
+    fun testautoRemoveContact {
         val user1 = User("id1","Ronald", "McDonald", mutableListOf<String>(), mutableListOf<Conversation>(), mutableListOf<User>())
         val user2 = User("id2","Pooh", "Bear", mutableListOf<String>(), mutableListOf<Conversation>(), mutableListOf<User>())
         user1.addToBlockList(user2)
         Assert.assertEquals(false, (user1 in user2.contacts))
         Assert.assertEquals(false, (user2 in user1.contacts))
+    }
+
+    @Test
+    fun testFindBlockedContactById() {
+        val blockedUserId = "blocked_id"
+        val randomId = "random_id"
+        val emptyId = ""
+        val user1 = User()
+
+        // Success case
+        user1.addBlockedContact(blockedUserId)
+        assertTrue(user1.checkIfBlocked(blockedUserId))
+
+        // Fail case: non-blocked ID
+        assertFalse(user1.checkIfBlocked(randomId))
+
+        // Fail case: empty ID string
+        assertFalse(user1.checkIfBlocked(emptyId))
+    }
+
+    @Test
+    fun testDeleteBlockedContact() {
+        val blockedUserId = "blocked_id"
+        val randomId = "random_id"
+        val emptyId = ""
+        var listSize: Int
+        val user1 = User()
+
+        // Success case
+        user1.addBlockedContact(blockedUserId)
+        listSize = user1.blockedContacts.size
+        assertTrue(user1.deleteBlockedContact(blockedUserId))
+        assertEquals(listSize - 1, user1.blockedContacts.size)
+
+        // Fail case: already removed ID
+        listSize = user1.blockedContacts.size
+        assertFalse(user1.deleteBlockedContact(blockedUserId))
+        assertEquals(listSize, user1.blockedContacts.size)
+
+        // Fail case: non-blocked ID
+        listSize = user1.blockedContacts.size
+        assertFalse(user1.deleteBlockedContact(randomId))
+        assertEquals(listSize, user1.blockedContacts.size)
+
+        // Fail case: empty ID string
+        listSize = user1.blockedContacts.size
+        assertFalse(user1.deleteBlockedContact(emptyId))
+        assertEquals(listSize, user1.blockedContacts.size)
     }
 }
