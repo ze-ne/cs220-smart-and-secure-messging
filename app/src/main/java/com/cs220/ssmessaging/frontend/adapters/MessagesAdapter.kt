51ec6f2843247c7cdb8adc9fbf1db6b7046f6e48
Zene Sekou <zene@cs.uchicago.edu>
2019-11-21 13:59:18 -0600

    fix time in msgs

--- app/src/main/java/com/cs220/ssmessaging/frontend/adapters/MessagesAdapter.kt
+++ app/src/main/java/com/cs220/ssmessaging/frontend/adapters/MessagesAdapter.kt
@@ -9,16 +9,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.cs220.ssmessaging.MyApplication.MyApplication
 import com.cs220.ssmessaging.R
 import com.cs220.ssmessaging.clientBackend.TextMessage
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 private const val MY_MESSAGE = 1
 private const val OTHER_MESSAGE = 2
 
-class MessagesAdapter(val context: Context, val messages: ArrayList<TextMessage>) : RecyclerView.Adapter<MessageViewHolder>() {
-
-    fun addMessage(message: TextMessage){
-        messages.add(message)
-        notifyDataSetChanged()
-    }
+class MessagesAdapter(val context: Context, val messages: ArrayList<TextMessage>) :
+    RecyclerView.Adapter<MessageViewHolder>() {
 
     override fun getItemCount(): Int {
         return messages.size
@@ -27,19 +27,30 @@ class MessagesAdapter(val context: Context, val messages: ArrayList<TextMessage>
     override fun getItemViewType(position: Int): Int {
         val message = messages[position]
 
-        return if(MyApplication.currentUser?.userId == message.senderId) {
+        return if (MyApplication.currentUser?.userId == message.senderId) {
             MY_MESSAGE
-        }
-        else {
+        } else {
             OTHER_MESSAGE
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
-        return if(viewType == MY_MESSAGE) {
-            MyMessageViewHolder(LayoutInflater.from(context).inflate(R.layout.item_my_message, parent, false))
+        return if (viewType == MY_MESSAGE) {
+            MyMessageViewHolder(
+                LayoutInflater.from(context).inflate(
+                    R.layout.item_my_message,
+                    parent,
+                    false
+                )
+            )
         } else {
-            OtherMessageViewHolder(LayoutInflater.from(context).inflate(R.layout.item_other_message, parent, false))
+            OtherMessageViewHolder(
+                LayoutInflater.from(context).inflate(
+                    R.layout.item_other_message,
+                    parent,
+                    false
+                )
+            )
         }
     }
 
@@ -48,27 +59,35 @@ class MessagesAdapter(val context: Context, val messages: ArrayList<TextMessage>
         holder.bind(message)
     }
 
-    inner class MyMessageViewHolder (view: View) : MessageViewHolder(view) {
+    fun getDate(milliSeconds: Long, dateFormat: String): String {
+        val formatter = SimpleDateFormat(dateFormat)
+
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = milliSeconds
+        return formatter.format(calendar.time)
+    }
+
+    inner class MyMessageViewHolder(view: View) : MessageViewHolder(view) {
         private var messageText: TextView = view.findViewById(R.id.my_message_text)
         private var timeText: TextView = view.findViewById(R.id.my_message_time)
 
         override fun bind(message: TextMessage) {
             messageText.text = message.message
-            timeText.text = (message.timestamp).toString()
+            timeText.text = getDate(message.timestamp, "MMM dd, hh:mma")
         }
     }
 
-    inner class OtherMessageViewHolder (view: View) : MessageViewHolder(view) {
+    inner class OtherMessageViewHolder(view: View) : MessageViewHolder(view) {
         private var messageText: TextView = view.findViewById(R.id.other_message_text)
         private var timeText: TextView = view.findViewById(R.id.other_message_time)
 
         override fun bind(message: TextMessage) {
             messageText.text = message.message
-            timeText.text = (message.timestamp).toString()
+            timeText.text = getDate(message.timestamp, "MMM dd, hh:mma")
         }
     }
 }
 
-open class MessageViewHolder (view: View) : RecyclerView.ViewHolder(view) {
-    open fun bind(message:TextMessage) {}
+open class MessageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    open fun bind(message: TextMessage) {}
 }
