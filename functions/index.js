8700738717db79ff2c41047015b946c70f32b918
Dona Ganzorig <ganzorig0518@uchicago.edu>
2019-11-25 21:42:41 -0600

    Using prefix for cloud function deletion rather than iteration

--- functions/index.js
+++ functions/index.js
@@ -7,27 +7,8 @@ const db = admin.firestore();
 exports.deleteConversationImages = functions.firestore
     .document('conversations/{conversationId}')
     .onDelete(async (snap, context) => {
-        return db.collection("conversations")
-            .doc(context.params.conversationId)
-            .collection('messages')
-            .get()
-            .then(snapshot => {
-                const bucket = firebase.storage().bucket();
-                let errorPaths = []
-                snapshot.forEach(doc => {
-                    const data = doc.data();
-                    if (data.message_type === "image") {
-                        const path = data.bucket_path;
-                        bucket.child(path).delete().then(() => {
-                            console.log("File: " + bucketImgPath + " deleted successfully");
-                            return;
-                        }).catch(err => {
-                            console.log("Error deleting: " + bucketImgPath + ". " + err);
-                            errorPaths.push(bucketImgPath);
-                            return;
-                        });
-                    }
-                });
-                return errorPaths;
-            });
+        const bucket = firebase.storage().bucket();
+        return bucket.deleteFiles({
+            prefix: `${context.conversationId}/`
+        });
     });
