387406f1455154ea59c30b7f758aa4c826b965d5
Dona Ganzorig <ganzorig0518@uchicago.edu>
2019-11-25 22:02:31 -0600

    Some more cloud function debugging

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,17 @@ const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 
 const firebase = admin.initializeApp();
-const db = admin.firestore();
+// const db = admin.firestore();
 
 exports.deleteConversationImages = functions.firestore
     .document('conversations/{conversationId}')
     .onDelete(async (snap, context) => {
-        const bucket = firebase.storage().bucket();
-        return bucket.deleteFiles({
-            prefix: `${context.conversationId}/`
+        const folderPath = `${context.conversationId}`;
+        const ref = firebase.storage().ref(folderPath);
+        return ref.listAll().then(dir => {
+            dir.items.forEach(fileRef => {
+                this.deleteFile(ref.fullPath, fileRef.name);
+            });
+            return;
         });
     });
