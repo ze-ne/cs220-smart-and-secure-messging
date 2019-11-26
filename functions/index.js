bec5617ac9ab6fda3f52ed8847e99b31909944fc
Dona Ganzorig <ganzorig0518@uchicago.edu>
2019-11-25 22:17:15 -0600

    Got deleteConversationImages cloud function fully working

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,15 @@
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const { Storage } = require('@google-cloud/storage');
 
 const firebase = admin.initializeApp();
-// const db = admin.firestore();
+const storage = new Storage();
+const bucket = storage.bucket('private-chat-client.appspot.com');
 
 exports.deleteConversationImages = functions.firestore
     .document('conversations/{conversationId}')
     .onDelete(async (snap, context) => {
-        const folderPath = `${context.conversationId}`;
-        const ref = firebase.storage().ref(folderPath);
-        return ref.listAll().then(dir => {
-            dir.items.forEach(fileRef => {
-                this.deleteFile(ref.fullPath, fileRef.name);
-            });
-            return;
+        return bucket.deleteFiles({
+            prefix: `${context.params.conversationId}/`
         });
     });
