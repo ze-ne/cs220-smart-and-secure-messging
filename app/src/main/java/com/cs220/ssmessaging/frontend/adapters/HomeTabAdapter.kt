1d6f41a1bb69f72f5b11bea1bf6ba2988bbfe946
Zene Sekou <zene@cs.uchicago.edu>
2019-11-26 19:06:47 -0600

    remove contacts screen

--- app/src/main/java/com/cs220/ssmessaging/frontend/adapters/HomeTabAdapter.kt
+++ app/src/main/java/com/cs220/ssmessaging/frontend/adapters/HomeTabAdapter.kt
@@ -4,28 +4,25 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.cs220.ssmessaging.frontend.fragments.BlockedListFragment
-import com.cs220.ssmessaging.frontend.fragments.ContactsFragment
 import com.cs220.ssmessaging.frontend.fragments.ConversationsListFragment
 
 class HomeTabAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
     override fun getItem(position: Int): Fragment {
         return when (position) {
             0 -> ConversationsListFragment()
-            1 -> ContactsFragment()
-            2 -> BlockedListFragment()
+            1 -> BlockedListFragment()
             else -> ConversationsListFragment()
         }
     }
 
     override fun getCount(): Int {
-        return 3
+        return 2
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
         return when (position) {
             0 -> "Conversations"
-            1 -> "Contacts"
-            2 -> "Blocked Users"
+            1 -> "Blocked Users"
             else -> ""
         }
     }
